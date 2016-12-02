50a9350a9a340368d560865c71eee0776d762755
Nam Nguyen Thanh <namnt3108@gmail.com>
2016-12-02 10:48:20 +0700

    Removed unused libs

--- angular-quickstart/assets/js/components/doctors/doctor.model.js
+++ angular-quickstart/assets/js/components/doctors/doctor.model.js
@@ -5,4 +5,4 @@ var Doctor = (function () {
     return Doctor;
 }());
 exports.Doctor = Doctor;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZG9jdG9yLm1vZGVsLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vLi4vYXBwL2NvbXBvbmVudHMvZG9jdG9ycy9kb2N0b3IubW9kZWwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBO0lBQUE7SUFHQSxDQUFDO0lBQUQsYUFBQztBQUFELENBQUMsQUFIRCxJQUdDO0FBSFksY0FBTSxTQUdsQixDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIERvY3RvciB7XHJcbiAgICBJZDogbnVtYmVyIHwgbnVsbDtcclxuICAgIE5hbWU6IHN0cmluZztcclxufVxyXG4iXX0=
+//# sourceMappingURL=doctor.model.js.map
